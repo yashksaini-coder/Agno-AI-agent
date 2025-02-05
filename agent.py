9b1cfaf1b977f8d97c39aae51545c6bd897dea7e
yashksaini-coder <ys3853428@gmail.com>
2025-02-05 17:44:53 +0530

    feat: 🎸 Multi AI agent completed

---
+++ agent.py
@@ -0,0 +1,50 @@
+from agno.agent import Agent
+from agno.models.groq import Groq
+from agno.tools.yfinance import YFinanceTools
+from agno.tools.duckduckgo import DuckDuckGoTools
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+GROQ_API_KEY = "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"
+
+# Web searching agent
+web_search_agent = Agent(
+    name="web_agent",
+    role="search the web for information based on the user given input",
+    model=Groq(id="llama-3.3-70b-versatile",api_key=GROQ_API_KEY),
+    tools=[
+        DuckDuckGoTools()
+    ],
+    show_tool_calls=False,
+    markdown=True,
+)
+
+financial_agent = Agent(
+    name="financial_agent",
+    role="get financial information",
+    model=Groq(id="llama-3.3-70b-versatile",api_key=GROQ_API_KEY),
+    tools=[
+        YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True)
+    ],
+    instructions=[
+        "Please provide the stock ticker symbol you would like to get information on",
+        "use tables to display the data",
+    ],
+    show_tool_calls=True,
+    markdown=True,
+)
+
+multiple_agents = Agent(
+    team=[web_search_agent, financial_agent],
+    model=Groq(id="llama-3.3-70b-versatile",api_key=GROQ_API_KEY),
+    markdown=True,
+    instructions=[
+        "Always include and show source data",
+        "Please provide the stock ticker symbol you would like to get information on",
+        "use tables to display the data",
+    ],
+)
+
+
+multiple_agents.print_response("summarize the stock market today for the stock AAPL")
